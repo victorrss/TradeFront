c413e4468b09cfe88e8a7936613137182c5427ff
Victor Rubens <victorrss@live.com>
2016-11-26 18:00:07 -0200

    começo de entidade Empresa

--- js/rest/empresa.js
+++ js/rest/empresa.js
@@ -1,17 +1,13 @@
-//var url = 'https://private-5b99d-tradeplace.apiary-mock.com/mercados';
-var url = 'http://localhost:8080/TradeForce/mercado';
+//var url = 'https://private-5b99d-tradeplace.apiary-mock.com/empresas';
+var url = 'http://localhost:8080/TradeForce/empresa';
 
 var nome ;
 var razaoSocial ;
-var endereco ;
-var lat ;
-var lng ;
-var mostrarMapa = false;
-
+var cnpj;
+var empresa = new Object();
 
 $(document).ready(function ($) {
-	mostrarMapa = false;
-	if (window.location.href.match("editar-mercado.html")) {
+	if (window.location.href.match("editar-empresa.html")) {
 		var id = getUrlVars()["id"];
 		if (id == null) {
 			window.location="index.html";
@@ -24,22 +20,14 @@ $(document).ready(function ($) {
 
 function CallbackListarPorID(data) {
 	ajaxindicatorstop()
-		mercado = {};
-		mercado.nome = data.nome;
-		mercado.endereco = data.endereco;
-		mercado.razaoSocial = data.razaoSocial;
-		mercado.latitude = data.localizacao.latitude;
-		mercado.longitude = data.localizacao.longitude;
-		$('#nome').val(mercado.nome);
-		$('#razaoSocial').val(mercado.razaoSocial);
-		$('#userEndereco').val(mercado.endereco);
-		$('#txtEndereco').val(mercado.endereco);
-		$('#txtLatitude').val(mercado.latitude);
-		$('#txtLongitude').val(mercado.longitude);
-		setEditPointMap(mercado.latitude,mercado.longitude);
-		clickMostrarMapa();
-		//$("#btnEndereco").click();
-  return mercado;
+		empresa = {};
+		empresa.nome = data.nome;
+		empresa.razaoSocial = data.razaoSocial;
+		empresa.cnpj = data.cnpj;
+		$('#nome').val(empresa.nome);
+		$('#razaoSocial').val(empresa.razaoSocial);
+		$('#cnpj').val(empresa.cnpj);
+  return empresa;
 }
 
 function RESTlistarPorID(id){
@@ -72,22 +60,8 @@ function RESTdeletar(id) {
 function RESTlistar() {
 	ajaxindicatorstart('Aguarde');
 	$.getJSON(url, function (data) {
-		for (var i = 0; i < data.length; i++) {
-			var linhaMercado = '<div class="linha-dados table" id="listMercado">'
-				+ '<div class="td lista-40">'
-				+ data[i].nome
-				+ '</div>'
-				+ '<div class="td space"></div>'
-				+ '<div class="td">'
-				+ data[i].endereco
-				+ '</div>'
-				+ '<div class="td space"></div>'
-				+ '<div class="td icons-edit">'
-				+ '<a href="editar-mercado.html?id='+ data[i].id +'"><i class="fa fa-pencil" aria-hidden="true"></i></a>'
-				+ '<a href="javascript: excluir('+ data[i].id +');"><i class="fa fa-trash" aria-hidden="true"></i></a>'
-				+ '</div>'
-				+ '</div>';
-			$(".lista-dados").append(linhaMercado);
+		empresa = data;
+		return empresa;
 		}
 		loadPaginacao()
 		ajaxindicatorstop()
@@ -100,14 +74,14 @@ function RESTlistar() {
 	});
 }
 
-function RESTinserir(nome, razaoSocial, endereco, lat, lng) {
-	//console.log(nome + ' ' + razaoSocial + ' ' + endereco + ' ' + lat + ' ' + lng);
+function RESTinserir(nome, razaoSocial, cnpj) {
+	//console.log(nome + ' ' + razaoSocial + ' ' + cnpj);
 	return $.ajax({
 		//async: false,
 		global: true,
 		type: 'POST',
 		url: url,
-		data: "{'endereco': '" + endereco + "','nome':'" + nome + "','razaoSocial': '" + razaoSocial + "', 'localizacao': {'latitude': '" + lat + "','longitude': '" + lng + "'}}",
+		data: '{"nome":"' + nome + '","razaoSocial": "' + razaoSocial + '","cnpj": "' + cnpj + '"}}',
 		contentType: "application/json; charset=UTF-8",
 		error: function (jqXHR, textStatus, errorThrown) {
 			/*if(textStatus==="timeout") {
@@ -122,13 +96,13 @@ function RESTinserir(nome, razaoSocial, endereco, lat, lng) {
 	});
 }
 
-function RESTeditar(id, nome, razaoSocial, endereco, lat, lng) {
+function RESTeditar(id, nome, razaoSocial, cnpj) {
 	return $.ajax({
 		//async: false,
 		global: true,
 		type: 'PUT',
 		url: url+'/'+id,
-		data: '{"endereco": "' + endereco + '","nome":"' + nome + '","razaoSocial": "' + razaoSocial + '", "localizacao": {"latitude": "' + lat + '","longitude": "' + lng + '"}}',
+		data: '{"nome":"' + nome + '","razaoSocial": "' + razaoSocial + '","cnpj": "' + cnpj + '"}}',
 		contentType: "application/json; charset=UTF-8",
 		error: function (jqXHR, textStatus, errorThrown) {
 			$.MessageBox({
@@ -157,7 +131,7 @@ function handleData(data, textStatus, jqXHR,acao) {
 				customClass: "custom_messagebox",
 				message: "Atualizado com sucesso!"
 			}).done(function(data, button){
-					window.location="mercados.html";
+					window.location="empresas.html";
 				});
 		}else	if (acao == 'excluir') {
 				$.MessageBox({
@@ -178,12 +152,17 @@ function listarPorId(id) {
 	RESTlistarPorID(id);
 }
 
+function listar(){
+	RESTEmpresa = RESTlistar();
+	console.log(RESTEmpresa);
+}
+
 function excluir(id) {
 	$.MessageBox({
 		customClass: "custom_messagebox",
 		buttonDone  : "Sim",
 		buttonFail  : "Não",
-		message     : "Você tem certeza que deseja EXCLUIR o mercado de ID " + id+"?"
+		message     : "Você tem certeza que deseja EXCLUIR a empresa de ID " + id+"?"
 	}).done(function(){
 		RESTdeletar(id).done(function(data, textStatus, jqXHR) {
 			handleData(data, textStatus, jqXHR, 'excluir');
@@ -195,10 +174,8 @@ function editar(id) {
 	if (validacaoForm() != true) { return; }
 	nome = $('#nome').val();
 	razaoSocial = $('#razaoSocial').val();
-	endereco = $('#txtEndereco').val();
-	lat = $('#txtLatitude').val();
-	lng = $('#txtLongitude').val();
-	RESTeditar(id, nome, razaoSocial, endereco, lat, lng).done(function(data, textStatus, jqXHR) {
+	cnpj = $('#cnpj').val();
+	RESTeditar(id, nome, razaoSocial, cnpj).done(function(data, textStatus, jqXHR) {
 		handleData(data, textStatus, jqXHR, 'editar');
 	});
 
@@ -207,15 +184,11 @@ function editar(id) {
 function inserir() {
 	if (validacaoForm() != true) { return; }
 	//console.log(nome + " - " + razaoSocial + " - " + endereco + " - " + lat + " - " + lng);
-	RESTinserir(nome, razaoSocial, endereco, lat, lng).done(handleData);
-}
-
-function clickMostrarMapa() {
-	mostrarMapa = true;
+	RESTinserir(nome, razaoSocial, cnpj).done(handleData);
 }
 
 function validacaoForm() {
-	if ($('#nome').val().length < 5 || $('#razaoSocial').val().length < 5 || $('#userEndereco').val().length < 5) {
+	if ($('#nome').val().length < 5 || $('#razaoSocial').val().length < 5 || $('#cnpj').val().length < 5) {
 		$.MessageBox({
 			customClass: "custom_messagebox",
 			message: "Entrada inválida, preencha todos os campos!"
@@ -223,17 +196,8 @@ function validacaoForm() {
 		return false;
 	}
 
-	if (mostrarMapa == false) {
-		$.MessageBox({
-			customClass: "custom_messagebox",
-			message: "Você não validou o endereço com o Google, para isso, clique em <br /> <span>'Mostrar no mapa'</span>"
-		});
-		return false;
-	}
 	nome = $('#nome').val();
 	razaoSocial = $('#razaoSocial').val();
-	endereco = $('#txtEndereco').val();
-	lat = $('#txtLatitude').val();
-	lng = $('#txtLongitude').val();
+	cnpj = $('#cnpj').val();
 	return true;
 }
